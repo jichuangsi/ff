bf1d866838a0c347b10c84703749a35c93e9a752
jichuansi <6834403@qq.com>
2018-12-03 17:45:45 +0800

    新建项目

--- fintheircing-customer/src/main/java/cn/com/fintheircing/customer/FintheircingCustomerApplication.java
+++ fintheircing-customer/src/main/java/cn/com/fintheircing/customer/FintheircingCustomerApplication.java
@@ -1,12 +1,12 @@
-package cn.com.fintheircing;
+package cn.com.fintheircing.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FintheircingApplication {
+public class FintheircingCustomerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FintheircingApplication.class, args);
+		SpringApplication.run(FintheircingCustomerApplication.class, args);
 	}
 }
