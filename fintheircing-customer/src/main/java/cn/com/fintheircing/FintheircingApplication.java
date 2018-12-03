b5372c8531d773bca762a4ee087e5a22109e125a
jichuansi <6834403@qq.com>
2018-12-03 16:33:22 +0800

    名称更改

---
+++ fintheircing-customer/src/main/java/cn/com/fintheircing/FintheircingApplication.java
@@ -0,0 +1,12 @@
+package cn.com.fintheircing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintheircingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintheircingApplication.class, args);
+	}
+}
