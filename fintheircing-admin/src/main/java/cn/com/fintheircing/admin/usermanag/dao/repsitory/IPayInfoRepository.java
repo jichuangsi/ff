88bd93829fcd28df770a301227d60077441e88ff
Y-bear <m13392530780@163.com>
2019-01-16 11:20:20 +0800

    姚雄 ->追加保证金-2

---
+++ fintheircing-admin/src/main/java/cn/com/fintheircing/admin/usermanag/dao/repsitory/IPayInfoRepository.java
@@ -0,0 +1,7 @@
+package cn.com.fintheircing.admin.usermanag.dao.repsitory;
+
+import cn.com.fintheircing.admin.usermanag.entity.PayInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IPayInfoRepository extends JpaRepository<PayInfo,String> {
+}
